faa52740000d637328c50774a09ceed252775f8a
Derrick Gall <derrickorama@gmail.com>
2024-01-16 11:14:24 -0600

    Initial commit

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace NaturalProgression.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        static float sprintDuration = 0f;
+        static float sprintXP = 0f;
+
+        [HarmonyPatch("Start")]
+        [HarmonyPrefix]
+        static void getNightVision(ref PlayerControllerB __instance)
+        {
+            NaturalProgressionMod.playerRef = __instance;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void UpdatePatch()
+        {
+            if (NaturalProgressionMod.playerRef.isSprinting)
+            {
+                sprintDuration += Time.deltaTime;
+                NaturalProgressionMod.mls.LogInfo("starting sprint timer");
+            } else
+            {
+                if (sprintStart > 0f)
+                {
+                    NaturalProgressionMod.mls.LogInfo("done sprinting");
+                    sprintXP += ((Time.deltaTime * 1000) - sprintStart);
+
+                    NaturalProgressionMod.mls.LogInfo($"{Time.deltaTime.ToString()}");
+                    NaturalProgressionMod.mls.LogInfo($"{sprintStart.ToString()}");
+                    NaturalProgressionMod.mls.LogInfo($"{(Time.deltaTime - sprintStart).ToString()}");
+
+                    sprintStart = 0f;
+
+                    NaturalProgressionMod.mls.LogInfo($"sprintXP: {sprintXP.ToString()}");
+                }
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using NaturalProgression.Patches;
+using GameNetcodeStuff;
+
+namespace NaturalProgression
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    internal class NaturalProgressionMod : BaseUnityPlugin
+    {
+        private const string modAuthor = "derrickorama";
+        private const string modGUID = "derrickorama.NaturalProgression";
+        private const string modName = "Natural Progression";
+        private const string modVersion = "0.1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        public static ManualLogSource mls;
+        internal static NaturalProgressionMod Instance { get; private set; }
+
+        internal static PlayerControllerB playerRef;
+
+        private void Awake()
+        {
+            Instance = this;
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Patching");
+
+            harmony.PatchAll(typeof(NaturalProgressionMod));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
